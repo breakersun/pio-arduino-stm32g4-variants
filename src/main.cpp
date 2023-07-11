33454dc7730b109b303bd84eb9e944f58d3e0fc3
leosun <leosunsl@outlook.com>
2023-07-11 14:29:02 +0800

    print hello world

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,14 @@
 int myFunction(int, int);
 
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
 }
 
 void loop() {
+  Serial.println("Hello World!");
+  delay(1000);
   // put your main code here, to run repeatedly:
 }
 
